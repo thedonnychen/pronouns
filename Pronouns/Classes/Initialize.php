cd4eb506244308fc8f8b25505760efb56cdddd4c
thedonnychen <donnychencti@gmail.com>
2021-12-14 15:27:39 -0800

    Added comments, added display in frontend

--- Pronouns/Classes/Initialize.php
+++ Pronouns/Classes/Initialize.php
@@ -12,11 +12,14 @@ class Initialize
         add_action('edit_user_profile', [$this, 'showProfileFields']);
         add_action('personal_options_update', [$this, 'saveProfileFields']);
         add_action('edit_user_profile_update', [$this, 'saveProfileFields']);
+        add_filter('the_author', [$this, 'addPronounsToAuthor']);
+        add_filter('comment_author', [$this, 'addPronounsToAuthor']); // different themes use comment_author or get_comment_author
+        add_filter('get_comment_author', [$this, 'addPronounsToAuthor']);
     }
-
+    
     /**
-     * Adds the post slug to the body class, used to style individual pages
-     * @return string
+     * Adds pronoun field to user field
+     * @return bool
      */
     public function showProfileFields($user)
     {
@@ -32,8 +35,14 @@ class Initialize
             </tr>
         </table>
         <?php
+
+        return;
     }
 
+    /**
+     * Saves pronoun field
+     * @return bool
+     */
     public function saveProfileFields($user_id)
     {
         if (!current_user_can('edit_user', $user_id)) {
@@ -41,5 +50,31 @@ class Initialize
         }
         
         update_usermeta( $user_id, 'pronouns', $_POST['pronouns'] );
+
+        return true;
+    }
+
+    /**
+     * Changes Author and Comment names to include pronouns
+     * Changed on string level because each theme's comment/author theme files are different, to fit most use cases.
+     * @return string
+     */
+    public function addPronounsToAuthor($author)
+    {
+        // If field doesn't exist, keep default
+        if (!get_the_author_meta('pronouns')) {
+            return $author;
+        }
+
+        // Accounts for Post Author
+        if (!get_comment()) {
+            $author = $author . ' (' . get_the_author_meta('pronouns') . ')';
+            return $author;
+        }
+
+        // Accounts for Post Comment
+        $author = $author . ' (' . get_the_author_meta('pronouns', get_comment()->user_id) . ')';
+
+        return $author;
     }
 }
